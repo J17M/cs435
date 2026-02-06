14bcbf7f0c803814f696d307b8ed2f6109603566
JM17 <juan17monarrezg@gmail.com>
2026-02-05 22:43:42 -0600

    created ciphers folder and utils.py to avoid copying the same code

---
+++ ciphers/caesar.py
@@ -0,0 +1,7 @@
+# random_cipher.py
+from .utils import ALPHABET 
+
+def encrypt(text):
+    # Example of using the imported string
+    # This prints: "The alphabet is: ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    print(f"The alphabet is: {ALPHABET}")
