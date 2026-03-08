f015a158247512e766aa9d098d7cc83f78e1de68
JM17 <juan17monarrezg@gmail.com>
2026-03-08 05:03:29 -0500

    Fully implemented caesar cipher... for now .......

--- ciphers/caesar.py
+++ ciphers/caesar.py
@@ -16,20 +16,47 @@ def encrypt_caesar(plaintext, shift):
             encrypted_text.append(char) # keep everything that isnt the alphabet    
     return "".join(encrypted_text)   
 
-    
+
+def decrypt_caesar(ciphertext, shift):
+    decrypted_text = []
+
+    for char in ciphertext:
+        if char.isalpha():
+            charUpper  = char.isupper()
+            indexChar = alphabet.index(char.upper())
+            shiftedText = alphabet[(indexChar - shift) % 26]
+            decrypted_text.append(shiftedText if charUpper else shiftedText.lower())
+        else:
+            decrypted_text.append(char)
+    return "".join(decrypted_text)
 
 if __name__ == "__main__":
-    user_plaintext = input("Input your plaintext: ")
-
-    # While loop to ensure proper input validation, catch errors when trying to conver to int
     while True:
-        user_shift = input("Input your key (shift): ")
-        try:
-            user_shift = int(user_shift)
-            break # if valid, exit loop
-        except ValueError:
-            print("Input is not an integer, try again\n")
-
-    print(encrypt_caesar(user_plaintext, user_shift))
+        mode = input("Encrypt or Decrypt? (e/d): ").strip().lower()
+        if mode == "e":
+            user_plaintext = input("Input your plaintext: ")
+            # While loop to ensure proper input validation, catch errors when trying to convert to int
+            while True:
+                user_shift = input("Input your key (shift): ")
+                try:
+                    user_shift = int(user_shift)
+                    break  # if valid, exit loop
+                except ValueError:
+                    print("Input is not an integer, try again\n")
+            print(encrypt_caesar(user_plaintext, user_shift))
+            break
+        elif mode == "d":
+            user_ciphertext = input("Input your ciphertext: ")
+            while True:
+                user_shift = input("Input your key (shift): ")
+                try:
+                    user_shift = int(user_shift)
+                    break
+                except ValueError:
+                    print("Input is not an integer, try again\n")
+            print(decrypt_caesar(user_ciphertext, user_shift))
+            break
+        else:
+            print("Invalid Mode, enter only 'e' or 'd'\n")
     
     
